ec32bd2c617c025b41255391a40254a369f195a8
pooja <morepooja811@gmail.com>
2024-12-01 11:47:16 +0530

    Revert "Revert "Secured the app with Basic auth""
    This reverts commit f6e6170457e9cf61e894e42f9d7a839239179d80.

--- JobApp/src/main/java/com/app/config/SecurityConfig.java
+++ JobApp/src/main/java/com/app/config/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.app.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -10,13 +8,17 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @EnableWebSecurity
+@Slf4j
 public class SecurityConfig {
 
 	@Autowired
@@ -25,6 +27,7 @@ public class SecurityConfig {
 	
 	@Bean
 	AuthenticationProvider authProvider() {
+		log.info("inside authProvider");
 		DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
 		provider.setUserDetailsService(userDetailsService);
 		provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
@@ -33,13 +36,12 @@ public class SecurityConfig {
 		
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-		http.csrf(customizer -> customizer.disable())
+		log.info("inside securityFilterChain");
+		return http.csrf(AbstractHttpConfigurer::disable)
 				.authorizeHttpRequests(request -> request.anyRequest().authenticated())
 				.httpBasic(Customizer.withDefaults())
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
 
-		return http.build();
 	}
 		
 }
